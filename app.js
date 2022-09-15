e063c334885ce099d10fbdfbeefea65699386aa0
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-14 18:50:26 -0600

    DOM elements and state established (maybe) and CSS

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 
 /* Get DOM Elements */
 let cityName = document.getElementById('city-name');
+let climateSelect = document.getElementById('climate-selector');
+let archSelector = document.getElementById('architecture-selector');
+let attractionsInput = document.getElementById('attractions');
+let climateDisplay = document.getElementById('climate-display');
+let archDisplay = document.getElementById('architecture-display');
+let attractionsDisplay = document.getElementById('attractions-display');
 
 /* State */
-
+const city = {
+    name: 'townsylvania',
+    climate: 'balmy',
+    architecture: 'arch heavy',
+    attractions: [],
+};
 /* Events */
 
 /* Display Functions */
