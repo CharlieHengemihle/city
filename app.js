b8b7332c9cb98ee248fa18dade20e9f98be85d4c
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-15 15:42:40 -0600

    not sure if I've commited in a while

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 
 /* Get DOM Elements */
 let cityName = document.getElementById('city-name');
+let nameDisplay = document.getElementById('name-zone');
 let climateSelect = document.getElementById('climate-selector');
-let archSelector = document.getElementById('architecture-selector');
+let archSelect = document.getElementById('architecture-selector');
 let attractionsInput = document.getElementById('attractions');
 let climateDisplay = document.getElementById('climate-display');
 let archDisplay = document.getElementById('architecture-display');
 let attractionsDisplay = document.getElementById('attractions-display');
-let climateImage = document.getElementById('');
-let archImage = document.getElementById('');
-let attractionsImage = document.getElementById('');
+let climateImage = document.getElementById('climate-display');
+let archImage = document.getElementById('architecture-display');
+let attractionsList = document.getElementById('attractions-display');
+let addButton = document.getElementById('att-button');
 
 /* State */
 const city = {
@@ -20,17 +22,42 @@ const city = {
     attractions: [],
 };
 /* Events */
+cityName.addEventListener('input', () => {
+    city.name = cityName.value;
+    displayCity();
+});
+
 climateSelect.addEventListener('change', () => {
     city.climate = climateSelect.value;
     displayCity();
 });
 
-architectureSelect.addEventListener('change', () => {
-    city.architecture = architectureSelect.value;
+archSelect.addEventListener('change', () => {
+    city.architecture = archSelect.value;
     displayCity();
 });
 /* Display Functions */
+function displayAttractions() {
+    attractionsList.innerHTML = '';
+
+    for (const attractions of city.attractions) {
+        const li = document.createElement('li');
+        li.textContent = attractions;
+        attractionsList.append(li);
+    }
+}
+
+addButton.addEventListener('click', () => {
+    const attractions = attractionsInput.value;
+    city.attractions.push(attractions);
+    displayAttractions();
+    attractionsInput.value = '';
+});
+
 function displayCity() {
-    climateImage.src = 'assets/city' + city.climate + '.jpg';
+    nameDisplay.textContent = city.name;
+
+    climateImage.src = 'assets/city/' + city.climate + '.jpg';
+    archImage.src = 'assets/city/' + city.architecture + '.jpg';
 }
 // (don't forget to call any display functions you want to run on page load!)
