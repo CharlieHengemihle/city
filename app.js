5f4e1a7d43b3274667c5532e064a3da21ccf3566
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-15 10:31:58 -0600

    images added

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ let attractionsInput = document.getElementById('attractions');
 let climateDisplay = document.getElementById('climate-display');
 let archDisplay = document.getElementById('architecture-display');
 let attractionsDisplay = document.getElementById('attractions-display');
+let climateImage = document.getElementById('');
+let archImage = document.getElementById('');
+let attractionsImage = document.getElementById('');
+
 
 /* State */
 const city = {
@@ -17,7 +21,13 @@ const city = {
     attractions: [],
 };
 /* Events */
-
+climateSelect.addEventListener('change'. () => {
+    city.climate = climateSelect.value;
+    displayCity();
+}):
 /* Display Functions */
+function displayCity() {
 
+
+}
 // (don't forget to call any display functions you want to run on page load!)
